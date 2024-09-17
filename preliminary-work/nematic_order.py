5d2a7a875533d55b756d6b28325a012f36222c6c
Steph McCallum <stephaniemccallu@u.boisestate.edu>
2024-09-17 11:35:37 -0600

    Moving a few lines inside loop

--- preliminary-work/nematic_order.py
+++ preliminary-work/nematic_order.py
@@ -7,12 +7,11 @@ import freud
 
 def nematic_order(gsd_file):
 
-    bond_vectors = []
     nm_orders = []
-    nematic = freud.order.Nematic() 
-    
     with gsd.hoomd.open(gsd_file) as traj: #opening gsd file as traj
         for snap in traj: #looping through each frame
+            bond_vectors = []
+            nematic = freud.order.Nematic() 
             for bond in snap.bonds.group: #looping through each bond in frame
                 pos2 = snap.particles.position[bond[1]]
                 pos1 = snap.particles.position[bond[0]]
